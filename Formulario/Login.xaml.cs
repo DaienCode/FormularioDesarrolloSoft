edbdd26f645114ebd2d45a437adda6213d5e8d7b
DavisSnow002 <nieveshernandezjesusdavid@gmail.com>
2022-05-10 23:23:58 -0500

    first commit

---
+++ Formulario/Login.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formulario
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            String Usuario = User.Text;
+            String Contraseña = Password.Text;
+
+            if (Usuario == "admin" && Contraseña == "admin")
+            {
+                Navigation.PushAsync(new Dashboard());
+            }else
+            {
+                DisplayAlert("Error", "Usuario o contraseña incorrectos", "OK");
+            }
+        }
+    }
+}
